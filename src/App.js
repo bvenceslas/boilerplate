fb6d9af2cf89606b436084d98ac5bf22164e292d
bvenceslas <bvenceslas@gmail.com>
2020-11-04 16:14:11 +0200

    Fix Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
-import Home from './components/Home';
+import Home from './Pages/Home';
 
 export default Home;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import BlogPostOne from '../BlogPostOne';
-import Header from '../Header';
-import Landing from '../Landing';
-import SubHeader from '../SubHeader';
+import BlogPostOne from '../../components/BlogPostOne';
+import Header from '../../components/Header';
+import Landing from '../../components/Landing';
+import SubHeader from '../../components/SubHeader';
+import './index.css';
  const Home = () => {
      return (
          <div>
