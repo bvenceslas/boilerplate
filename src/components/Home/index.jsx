a61d5eb098787b7682a672d32667c615f52472c0
bvenceslas <bvenceslas@gmail.com>
2020-11-04 13:24:25 +0200

    Add Landing Image

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import Landing from '../Landing';
 import SubHeader from '../SubHeader';
  const Home = () => {
      return (
          <div>
              <Header />
              <SubHeader />
+             <Landing />
          </div>
      )
 }
