f32f44a1650d43d375458562164beb6275ee33c1
bvenceslas <bvenceslas@gmail.com>
2020-11-11 13:43:29 +0200

    Fix PostImage

--- src/components/PostImage/index.jsx
+++ src/components/PostImage/index.jsx
@@ -5,7 +5,7 @@ import Landing from '../../assets/images/landing.png';
 const PostImage = () => {
     return (
         <div className="image-post-container">
-            <img src={Landing}  alt="pic on post add" width="1200" height="500"/>
+            <img src={Landing}  alt="pic on post add" width="980" height="270"/>
         </div>
     )
 }
